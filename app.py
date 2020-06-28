ad612713e901a1bd317a55778330bebe226f73cb
Jordan Venderley <jmv234@cornell.edu>
2020-06-28 12:51:00 -0400

    build ticker page

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def get_ticker():
+  return render_template('ticker.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+#@app.route('/')
+#def index():
+#  return render_template('index.html')
+#
+#@app.route('/about')
+#def about():
+#  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
